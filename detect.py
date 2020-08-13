4b73db7efbfc73e9bae755746f8cefe83e558ba1
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-08-13 13:34:43 -0400

    Create detect.py

---
+++ detect.py
@@ -0,0 +1,57 @@
+import jetson.inference
+import jetson.utils
+
+import argparse
+import sys
+
+# parse the command line
+parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.")
+
+parser.add_argument("input_URI", type=str, default="", nargs='?', help="URI of the input stream")
+parser.add_argument("output_URI", type=str, default="", nargs='?', help="URI of the output stream")
+parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
+parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
+parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use") 
+
+
+strnetwork = 'ssd-inception-v2'
+outputFilename = 'file://image_%i.jpg'
+
+# load the object detection network
+net = jetson.inference.detectNet(strnetwork, sys.argv, 0.5)
+
+#jetbot csi://0
+#usb v4l2:///dev/video0
+
+# https://github.com/dusty-nv/jetson-inference/blob/master/docs/aux-streaming.md
+# 
+# create video sources & outputs
+input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
+output = jetson.utils.videoOutput(outputFilename, argv=sys.argv)
+
+# process frames until the user exits
+while True:
+	# capture the next image
+	img = input.Capture()
+
+	# detect objects in the image (with overlay)
+	detections = net.Detect(img, overlay=opt.overlay)
+
+	# print the detections
+	print("detected {:d} objects in image".format(len(detections)))
+
+	for detection in detections:
+		print(detection)
+
+	# render the image
+	output.Render(img)
+
+	# update the title bar
+	output.SetStatus("{:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkFPS()))
+
+	# print out performance info
+	net.PrintProfilerTimes()
+
+	# exit on input/output EOS
+	if not input.IsStreaming() or not output.IsStreaming():
+		break
